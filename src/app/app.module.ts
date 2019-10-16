239b2bad0e683b94a90636e8c342da02b4287864
Dmitrii Vasilev <dmitrii_vasilev@epam.com>
2019-10-16 11:50:40 +0400

    Add courses page component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,13 +6,15 @@ import { AppComponent } from './app.component';
 import { HeaderComponent } from './componets/header/header.component';
 import { FooterComponent } from './componets/footer/footer.component';
 import { LogoComponent } from './componets/logo/logo.component';
+import { CoursesPageComponent } from './components/courses-page/courses-page.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
     FooterComponent,
-    LogoComponent
+    LogoComponent,
+    CoursesPageComponent
   ],
   imports: [
     BrowserModule,
