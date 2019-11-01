ddb86c83d0d28af151e5de0893c97fd4ff04eaf8
Dmitrii Vasilev <Dmitrii_Vasilev@epam.com>
2019-11-02 01:12:43 +0400

    Change detection

--- src/app/components/course-item/course-item.component.ts
+++ src/app/components/course-item/course-item.component.ts
@@ -1,11 +1,13 @@
 import { Component, OnInit, Input, ViewEncapsulation, Output, EventEmitter } from '@angular/core';
 import { Course } from '../../interfaces/course';
+import { ChangeDetectionStrategy } from '@angular/compiler/src/core';
 
 @Component({
     selector: 'app-course-item',
     templateUrl: './course-item.component.html',
     styleUrls: ['./course-item.component.styl'],
     encapsulation: ViewEncapsulation.None,
+    changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class CourseItemComponent implements OnInit {
     @Input() course: Course;
