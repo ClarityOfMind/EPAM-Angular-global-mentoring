5c5c0b96acf99099f34161609f667e04e994f0ba
Dmitrii Vasilev <dmitrii_vasilev@epam.com>
2019-11-18 00:49:31 +0400

    Add course service

--- src/app/components/courses-page/courses-page.component.ts
+++ src/app/components/courses-page/courses-page.component.ts
@@ -1,4 +1,3 @@
-
 import {
     Component,
     ViewEncapsulation,
@@ -14,6 +13,7 @@ import {
 } from '@angular/core';
 import { Course } from '../../interfaces/course';
 import { FilterPipe } from '../../pipes/filter-pipe/filter.pipe';
+import { CourseService } from 'src/app/services/course-service/course-service.service';
 
 
 @Component({
@@ -22,83 +22,29 @@ import { FilterPipe } from '../../pipes/filter-pipe/filter.pipe';
     styleUrls: ['./courses-page.component.styl'],
     encapsulation: ViewEncapsulation.None,
 })
-export class CoursesPageComponent implements
-    OnInit,
-    DoCheck,
-    AfterContentInit,
-    AfterContentChecked,
-    AfterViewInit,
-    AfterViewChecked,
-    OnDestroy {
+export class CoursesPageComponent implements OnInit {
     public courses: Course[];
     public id: string;
     public list: Course[];
 
-    constructor(private filterPipe: FilterPipe) {
-        console.log('constructor');
-    }
+    constructor(
+        private filterPipe: FilterPipe,
+        private courseService: CourseService,
+    ) {}
 
     ngOnInit() {
-        this.courses = [
-            {
-                id: '1',
-                title: 'JavaScript Tutorial',
-                creationDate: '12.01.2019',
-                duration: '88',
-                description: `Learn about where you can find course descriptions, what information they include, how they work, and details about various components of a course description. Course descriptions report information about a university or college's classes. They're published both in course catalogs that outline degree requirements and in course schedules that contain descriptions for all courses offered during a particular semester`,
-                topRated: true,
-            },
-            {
-                id: '2',
-                title: 'Python Course',
-                creationDate: '11.01.2019',
-                duration: '50',
-                description: `Learn about where you can find course descriptions, what information they include, how they work, and details about various components of a course description. Course descriptions report information about a university or college's classes. They're published both in course catalogs that outline degree requirements and in course schedules that contain descriptions for all courses offered during a particular semester`,
-                topRated: false,
-            },
-            {
-                id: '3',
-                title: 'PHP Workshop',
-                creationDate: '09.11.2018',
-                duration: '79',
-                description: `Learn about where you can find course descriptions, what information they include, how they work, and details about various components of a course description. Course descriptions report information about a university or college's classes. They're published both in course catalogs that outline degree requirements and in course schedules that contain descriptions for all courses offered during a particular semester`,
-                topRated: false,
-            },
-        ];
-
-        this.list = this.courses;
-        console.log('init');
+        this.list = this.courseService.getList();
     }
 
-    ngDoCheck(): void {
-        console.log('do check');
-    }
-
-    ngAfterContentInit(): void {
-        console.log('after contet init');
-    }
-
-    ngAfterContentChecked(): void {
-        console.log('after contet checked');
-    }
-
-    ngAfterViewInit(): void {
-        console.log('after view init');
-    }
-
-    ngAfterViewChecked(): void {
-        console.log('after view checked');
-    }
-
-    ngOnDestroy(): void {
-        console.log('on destroy');
-    }
     public load(): void {
         console.log('loading...');
     }
 
     public deleteCourse(id: string): void {
-        console.log(id);
+        if (confirm('Do you really want to delete course?')) {
+            this.courseService.removeItem(id);
+            this.list = this.courseService.getList();
+        }
     }
 
     public searchCourse(name: string): void {
---
+++ src/app/services/course-service/course-service.service.ts
@@ -0,0 +1,62 @@
+import { Injectable } from '@angular/core';
+import { Course } from 'src/app/interfaces/course';
+
+@Injectable({
+    providedIn: 'root'
+})
+export class CourseService {
+    private courses = [
+        {
+            id: '1',
+            title: 'JavaScript Tutorial',
+            creationDate: '12.01.2019',
+            duration: '88',
+            description: `Learn about where you can find course descriptions, what information they include, how they work, and details about various components of a course description. Course descriptions report information about a university or college's classes. They're published both in course catalogs that outline degree requirements and in course schedules that contain descriptions for all courses offered during a particular semester`,
+            topRated: true,
+        },
+        {
+            id: '2',
+            title: 'Python Course',
+            creationDate: '11.01.2019',
+            duration: '50',
+            description: `Learn about where you can find course descriptions, what information they include, how they work, and details about various components of a course description. Course descriptions report information about a university or college's classes. They're published both in course catalogs that outline degree requirements and in course schedules that contain descriptions for all courses offered during a particular semester`,
+            topRated: false,
+        },
+        {
+            id: '3',
+            title: 'PHP Workshop',
+            creationDate: '09.11.2018',
+            duration: '79',
+            description: `Learn about where you can find course descriptions, what information they include, how they work, and details about various components of a course description. Course descriptions report information about a university or college's classes. They're published both in course catalogs that outline degree requirements and in course schedules that contain descriptions for all courses offered during a particular semester`,
+            topRated: false,
+        },
+    ];
+
+    constructor() { }
+
+    public getList(): Course[] {
+        return this.courses;
+    }
+
+    public createCourse(course: Course): void {
+        this.courses.push(course);
+    }
+
+    public getItemById(id: string): Course[] {
+        return this.courses.filter(course => course.id === id);
+    }
+
+    public updateItem(id: string): void {
+    }
+
+    public removeItem(id: string): void {
+        let index: number;
+
+        for (const course of this.courses) {
+            if (course.id === id ) {
+                index = this.courses.indexOf(course);
+            }
+        }
+        this.courses.splice(index, 1);
+    }
+}
