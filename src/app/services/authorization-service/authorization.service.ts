eb069df306bb88554b2b2ba6193fa3a34f190bd1
Dmitrii Vasilev <Dmitrii_Vasilev@epam.com>
2019-11-18 18:13:03 +0400

    Add auth service

--- src/app/services/authorization-service/authorization.service.ts
+++ src/app/services/authorization-service/authorization.service.ts
@@ -1,9 +1,29 @@
 import { Injectable } from '@angular/core';
+import { Authorization } from '../../interfaces/authorization';
 
 @Injectable({
-  providedIn: 'root'
+    providedIn: 'root'
 })
 export class AuthorizationService {
+    private currentUser: Authorization | null;
 
-  constructor() { }
+    constructor() {
+        this.currentUser = localStorage.getItem('userName') && JSON.parse(localStorage.getItem('userName'));
+    }
+
+    public login(auth: Authorization): void {
+        localStorage.setItem('userName', JSON.stringify(auth));
+    }
+
+    public logout(): void {
+        this.currentUser = null;
+    }
+
+    public getUserInfo(): void {
+        return this.currentUser = null;
+    }
+
+    public get isAuthenticated(): boolean {
+        return !!this.currentUser;
+    }
 }
