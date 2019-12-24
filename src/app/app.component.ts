82d319bac5a6765c071d5de348f3f3a650eb19c5
Andrey Kharitonov <Andrey_Kharitonov@epam.com>
2019-12-24 17:04:54 +0400

    Fixed Expression has changed after it was checked....

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,9 @@
-import {Component, OnInit, ViewEncapsulation} from '@angular/core';
+import {ChangeDetectorRef, Component, OnDestroy, OnInit, ViewEncapsulation} from '@angular/core';
 import {LoadingService} from './services/loading.service';
+import {AppState} from './store/app.reducers';
+import {State} from '@ngrx/store';
+import {AuthorizationService} from './services/authorization-service/authorization.service';
+import {Subscription} from 'rxjs';
 
 @Component({
     selector: 'app-root',
@@ -7,14 +11,39 @@ import {LoadingService} from './services/loading.service';
     styleUrls: ['./app.component.styl'],
     encapsulation: ViewEncapsulation.None
 })
-export class AppComponent implements OnInit{
-    public loading$: boolean;
+export class AppComponent implements OnInit, OnDestroy {
+    public loading = false;
+
+    private sub: Subscription;
 
     constructor(
         public loadingService: LoadingService,
+        private state: State<AppState>,
+        private authService: AuthorizationService,
+        private cdRef: ChangeDetectorRef
     ) {}
 
     ngOnInit(): void {
+        this.sub = this.loadingService
+            .loading$
+            .subscribe(loading => {
+                this.loading = loading;
 
+                //Без этого будет ошибка "Expression has changed after it was checked...."
+                this.cdRef.detectChanges();
+            });
+
+        if (this.state.getValue().auth.token) {
+            this.authService
+                .getUserInfo()
+                .subscribe();
+        }
+    }
+
+    ngOnDestroy(): void {
+        if (this.sub) {
+            this.sub.unsubscribe();
+            this.sub = null;
+        }
     }
 }
